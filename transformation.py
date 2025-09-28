839705f480c2d5fc8aa8b3da46854fbb62307d76
Thirunavukkarasu Narayanan <47706372+Thiru89@users.noreply.github.com>
2025-09-28 19:53:22 -0400

    Update transformation.py

--- transformation.py
+++ transformation.py
@@ -14,27 +14,27 @@ import pandas as pd
 
 #define tools the agent can use
 @tool
-def preview_data(table: str) -> str:
-    """Returns a preview of the data in the table."""
-    df = spark.read.table(table).limit(10).toPandas()
+def scan_data(table: str) -> str:
+    """scans the table to understand the metadata information"""
+    df = spark.read.table(table).limit(100).toPandas()
     return df.head()
 
 @tool
-def suggest_transformations(column_summary: str) ->str:
-    "This method suggests transformations based on column summary"
+def transformation_recommendation(table_summary: str) ->str:
+    "This method suggests required transformations recommendation"
     prompt = f"""
-    you are a data engineer assistant. based on the following column summary, suggest simple, short ETL transformation steps. but do not drop the columns
+    You are a data engineer assistant. based on the following table summary, suggest simple, ETL transformation steps if required. but do not drop the columns
     output format : each suggestion on a new line, without explanations or markdown.
-    Example: Remove $ from reveneue and cast to float Column summary:{column_summary}"""
+    Example: Remove blank spaces and cast to float Column summary:{table_summary}"""
     return llm.predict(prompt).strip()
 
 @tool
-def generate_spark_code(transform_description: str) ->str:
-    """Generate Pyspark code from transformation description."""
+def generate_pyspark_code(transform_dtls: str) ->str:
+    """Generate a pyspark code from transformation recommendation"""
     prompt = f"""
-    You are a data engineer. Write a pyspark code to apply the following ETL transformations to Dataframe called 'df'.
+    You are a data engineer. Write a pyspark code to apply the following basic transformations to Dataframe 'df'.
     Transformations:
-    {transform_description}
+    {transform_dtls}
     Only return the Pyspark code and overwrite if the target table exists. No explanation, no markdown."""
     return llm.predict(prompt).strip()
 
@@ -50,7 +50,7 @@ def extract_only_code(code_value: str) ->str:
     return llm.predict(prompt).strip()
 
 
-tools = [preview_data, suggest_transformations, generate_spark_code, extract_only_code]
+tools = [scan_data, transformation_recommendation, generate_pyspark_code, extract_only_code]
 
 #create Agent
 agent = initialize_agent(
@@ -62,7 +62,7 @@ agent = initialize_agent(
 )
 #run the agent
 response = agent.run(
-  "Preview the table mycatalog.myschema.customer_sample and generate Spark code to read the table, clean it, and finally write the dataframe into a table called mycatalog.myschema.customer_sample_clean and extract only the code and make sure syntax is correct. Do not stope the Spark Session ")
+  "scan or preview the table mycatalog.myschema.customer_sample and generate Spark code to read the table, cleanse it, and write the dataframe into a table called mycatalog.myschema.customer_sample_clean and extract only the code and make sure syntax is correct. Do not stope the Spark Session ")
 print("Generate Pyspark Code:\n")
 print(response)
 
